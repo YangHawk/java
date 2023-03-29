6985f58c33e4f07c35002a48ce2d987bca105789
itwill <itwill@DESKTOP-H8TE7NE>
2023-03-29 17:55:19 +0900

    2Day

--- java_1/src/basic/DataTypeApp.java
+++ java_1/src/basic/DataTypeApp.java
@@ -3,6 +3,38 @@ package basic;
 public class DataTypeApp {
 	public static void main(String[] args) {
 		System.out.println("<<정수형(IntegerType)>>");
-		System.out.println("정수형(10진수 100) = "+100);
+		System.out.println("정수값(10진수 100) = "+100);
+		System.out.println("정수값(8진수 100) = "+0100);
+		System.out.println("정수값(16진수 100) = "+0x100);
+		System.out.println("정수값(10진수 100) = "+100L);
+		System.out.println("정수값= "+2147483647);
+		System.out.println("정수값= "+2147483648L);
+
+		byte a1=127;
+		short a2=32767;
+		int a3=2147483647;
+		long a4=2147483648L;
+		
+		System.out.println("a1 = "+a1);
+		System.out.println("a2 = "+a2);
+		System.out.println("a3 = "+a3);
+		System.out.println("a4 = "+a4);
+		
+		long a5=100;
+		
+		System.out.println("a5 = "+a5);
+		System.out.println("==============================================");
+		System.out.println("<<실수형(DoubleType)>>");
+		System.out.println("실수값(8Byte) = "+12.3);
+		System.out.println("실수값(4Byte) = "+12.3F);
+		System.out.println("실수값(8Byte) = "+0.000000000123);
+		System.out.println("실수값(8Byte) = "+1.23E+10);
+
+		float b1=1.23456789F;
+		double b2=1.23456789;
+	
+		System.out.println("b1 = "+b1);
+		System.out.println("b2 = "+b2);
+		System.out.println("==============================================");
 	}
 }
--- java_1/src/basic/VariableApp.java
+++ java_1/src/basic/VariableApp.java
@@ -2,6 +2,31 @@ package basic;
 
 public class VariableApp {
 	public static void main(String[] args) {
+		int su;
+		su=100;
+		System.out.print("초기값 = ");
+		System.out.println(su);
+		su=200;
+		System.out.print("변경값 = ");
+		System.out.println(su);
 		
+		
+		System.out.println("==========================");
+		int num=100;
+		System.out.println("num = "+num);
+		System.out.println("올해는 "+2+0+2+3+"년입니다.");
+		System.out.println(2+0+2+3+"년은 토끼띠 해입니다.");
+		System.out.println(""+2+0+2+3+"년은 토끼띠 해입니다.");
+		int num1=100, num2=200;
+		System.out.println("연산결과 = "+num1+num2);
+		System.out.println("연산결과 = "+(num1+num2));
+		System.out.println("연산결과 = "+num1*num2);
+		System.out.println("연산결과 = "+(num1*num2));
+		int kor=88, eng=90;
+		int tot=kor+eng;
+		System.out.println("점수 합계 = "+tot);
+		System.out.println("==========================");
+		int count = 10;
+		System.out.println("count = "+count);
 	}
 }
