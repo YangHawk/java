21bc2f8a038d13ada77a31586e606aef2d5e09cb
user <user@BOOK-LCCUUB9T5H>
2023-07-26 18:18:52 +0900

    07/26

---
+++ spring/src/main/java/xyz/itwill04/bean/AnnotationConfigurationBean.java
@@ -0,0 +1,27 @@
+package xyz.itwill04.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration: 스프링 컨테이너에 의하여 관리될 객체(Spring Bean)를 생성하여 반환하는 메소드가 선언된 클래스를 설정하기 위한 어노테이션
+//▶ 클래스를 Spring Bean Configuration File과 유사한 기능을 제공할 수 있도록 설정하는 어노테이션
+@Configuration
+public class AnnotationConfigurationBean {
+	// @Bean: 클래스를 객체(Spring Bean)로 생성하여 반환하는 메소드의 설정하는 어노테이션
+	// ▶ @Bean 어노테이션을 사용한 메소드가 반환한 객체는 스프링 컨테이너에 의하여 관리됨 - Spring Bean
+	// ▶ Spring Bean Configuration File의 bean 엘리먼트와 유사한 기능을 제공하는 어노테이션
+	// ▶ 메소드의 이름이 Spring Bean의 식별자(beanName)로 사용됨
+	// ▶ @Bean 어노테이션의 name 속성을 이용하여 식별자(beanName) 변경 가능 - e.g: @Bean(name = "name")
+	@Bean
+	public AnnotationBean annotationBean() {
+		return new AnnotationBean();
+	}
+	/*
+	@Component 어노테이션을 이용!
+	
+	@Bean
+	public ComponentAnnotationBean componentAnnotationBean() {
+		return new ComponentAnnotationBean();
+	}
+	*/
+}
--- spring/src/main/java/xyz/itwill04/bean/CreateBeanApp.java
+++ spring/src/main/java/xyz/itwill04/bean/CreateBeanApp.java
@@ -2,14 +2,49 @@ package xyz.itwill04.bean;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 //Spring 프레임워크에서는 BeanFactory 객체 또는 ApplicationContext 객체로 String 컨테이너(Spring Container) 기능을 제공
 //▶ 스프링 컨테이너는 환경 설정 파일(Spring Bean Configuration File - XM<L) 파일을 제공받아 Spring Bean(객체) 관리
+@SuppressWarnings("deprecation")
 public class CreateBeanApp {
 	public static void main(String[] args) {
-		System.out.println("1.BeanFactory 객체를 생성하여 스프링 컨테이너로 사용하는 방법");
+		System.out.println("1. BeanFactory 객체를 생성하여 스프링 컨테이너로 사용하는 방법");
 		System.out.println("=============== Spring Container 초기화 전 ===============");
-		BeanFactory factory = new XmlBeanFactory(null);
+		// BeanFactory 인터페이스를 상속받은 자식 클래스로 객체를 생성 - BeanFactory 객체
+		// ▶ BeanFactory 객체를 생성할 때 Spring Bean Configuration File을 제공받아 스프링 컨테이너 생성 - 스프링 컨테이너 초기화 작업
+		// ▶ 매개 변수에는 Spring Bean Configuration File의 경로를 제공받아 Resource 객체로 생성하여 전달
+		// ▶ BeanFactory 객체는 Spring Bean Configuration File에 등록된 클래스로 미리 객체를 생성하지 않고 Spring Bean을 제공받기 위하여 요청 시 객체를 생성하여 제공(※실행해보면 아무런 동작 X)
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/04-1_beanCreate.xml"));
+		System.out.println("=============== Spring Container 초기화 후 ===============");
+		// BeanFactory.getBean(String beanName): 매개 변수에 Spring Bean을 구분하기 위한 식별자(beanName)을 전달하여 스프링 컨테이너가 Spring Bean(객체)를 생성하여 반환하는 메소드
+		// ▶ Object 타입의 객체를 반환하기 때문에 반드시 명시적 객체 형변환 사용
+		// ▶ 매개 변수로 전달받은 식별자(beanName)의 Spring Bean이 없을 경우 NoSuchBeanDefinitionException 예외 발생
+		CreateBean bean1 = (CreateBean) factory.getBean("createBean");
+		bean1.display();
+		System.out.println("==========================================================");
+		System.out.println("2. ApplicationContext 객체를 생성하여 스프링 컨테이너로 사용하는 방법");
 		System.out.println("=============== Spring Container 초기화 전 ===============");
+		// DL(Dependency Lookup): 스프링 컨테이너가 관리하는 객체(Spring Bean)를 검색하여 제공 
+		// ApplicationContext 인터페이스를 상속받은 자식 클래스로 객체 생성 - ApplicationContext 객체
+		// ▶ ApplicationContext 객체를 생성할 때 Spring Bean Configuration File을 제공받아 스프링 컨테이너 생성 - 스프링 컨테이너 초기화 작업
+		// ▶ 클래스가 참조 가능한 폴더(ClassPath)에 저장된 Spring Bean Configuration File을 제공받아 사용 가능
+		// ▶ ApplicationContext 객체는 Spring Bean Configuration File에 등록된 클래스로 미리 객체를 생성하여
+		// Spring Bean 요청 시 미리 생성된 객체를 제공
+		ApplicationContext context = new ClassPathXmlApplicationContext("04-1_beanCreate.xml");
+		System.out.println("=============== Spring Container 초기화 후 ===============");
+		// ApplicationContext.getBean(String beanName): 매개 변수로 Spring Bean을 구분하기 위한
+		// 식별자(beanName)을 전달받아 스프링 컨테이너로부터 Spring Bean(객체)를 반환하는 메소드
+		// ▶ Object 타입의 객체를 반환하기 때문에 반드시 명시적 객체 형변환 사용
+		// ▶ 매개 변수로 전달받은 식별자(beanName)의 Spring Bean이 없을 경우 NoSuchBeanDefinitionException 예외 발생
+		CreateBean bean2 = (CreateBean) context.getBean("createBean");
+		bean2.display();
+
+		// ClassPathXmlApplicationContext.close(): ApplicationContext 객체를 제거하는 메소드
+		// ▶ 스프링 컨테이너가 소멸되기 전에 스프링 컨테이너에 의하여 관리되는 모든 객체(Spring Bean)를 자동 소멸
+		((ClassPathXmlApplicationContext) context).close();
+		System.out.println("==========================================================");
 	}
 }
