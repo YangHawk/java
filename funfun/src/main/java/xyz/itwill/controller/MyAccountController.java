cf42c588989671203776f0d7b27bbe7a6f7e440c
Jin <Jin@DESKTOP-J2PS0I7>
2023-09-18 00:06:25 +0900

    09/18

--- funfun/src/main/java/xyz/itwill/controller/MyAccountController.java
+++ funfun/src/main/java/xyz/itwill/controller/MyAccountController.java
@@ -3,8 +3,11 @@ package xyz.itwill.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import xyz.itwill.security.CustomAccountDetails;
 
 
 @PreAuthorize("isAuthenticated()")
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/account")
 public class MyAccountController {
 	@RequestMapping(value = "/myaccount", method = RequestMethod.GET)
-	public String index(Authentication authentication) {
+	public String index(Authentication authentication, Model model) {
+		CustomAccountDetails loginAccount = (CustomAccountDetails)authentication.getPrincipal();
+		model.addAttribute("loginAccount", loginAccount);
 
 		return "account/myaccount";
 	}
